fe0e3c6a8485e06fcfdd6de86ac877a0ba4b4dba
Quentin.Walker <quentin.walker@noaa.gov>
2021-04-21 18:31:52 -0400

    Updated Intro to script Added functions at the start

--- Q10.R
+++ Q10.R
@@ -1,5 +1,31 @@
-### Q10
-# Traditional Q10
+#####
+# N.D. McTigue, Q.A. Walker, and C.A. Currin 2021
+# Refining estimates of greenhouse gas emissions from salt marsh “blue carbon” erosion and decomposition
+# email: quentin.walker@noaa.gov, mctigue@utexas.edu
+#####
+
+## Run this sript second ##
+
+##### Calculate Temperature Sensitivity #####
+# This script takes the summarized decomp expt data and calculates temp sensitivity 
+# for the shallow and deep depth horizons
+# temperature sensitivity is measured with traditional Q10 and Q10q
+
+# load required packages
+library(reshape2)
+
+# load in function to propogate error
+source(file.path(getwd(),"mutate_with_error.R"))
+
+# Function that calculates the time where we estimate each group of bottles crossed the threshold
+TimeAtGivenPortion <- function(Portion, BeforeXY, AfterXY){
+  m = (AfterXY[2]-BeforeXY[2])/(AfterXY[1]-BeforeXY[1]) #calculate slope (rise over run)
+  b = AfterXY[2]-m*AfterXY[1] #calculate the intercept by substituing the calculated slope into y=mx+b
+  Time = (Portion - b)/m #calculate the time the linear model intercepts the threshold value
+  return(Time) #return Time
+}
+
+## Traditional Q10 ####
 decompRate <- data.frame(Depth = NA, Treatment = NA, day = NA)
 
 for (d in unique(decomp.data$day)) {
@@ -32,10 +58,12 @@ Q10 <- decompRate %>%
   mutate(seQ10 = dQ10/sqrt(6)) %>% 
   select(day, Depth, dplyr::contains("Q"))
 
+# Plot Q10
 ggplot(Q10, aes(day, Q10, color = Depth))+
   geom_line()+
   geom_errorbar(aes(min = Q10 - seQ10, max = Q10 + seQ10))+
-  geom_point()
+  geom_point()+
+  theme_bw()
 
 #####
 # Q10q
@@ -70,7 +98,7 @@ for (q in seq(0, round(max(decomp.data$PercentCRespired), 2), by=0.01)) {
 }
 
 for (i in 1:nrow(ElapsedTime)) {
-  ElapsedTime$CalcTime[i] <- with(ElapsedTime[i, ], TimeAtGivenPortion(PortionC, c(TimeBefore, ValueBefore), c(TimeAfter, ValueAfter)))
+  ElapsedTime[i, "CalcTime"] <- with(ElapsedTime[i, ], TimeAtGivenPortion(PortionC, c(TimeBefore, ValueBefore), c(TimeAfter, ValueAfter)))
 }
 
 ElapsedTime <- ElapsedTime %>% 
@@ -87,7 +115,6 @@ ElapsedTime.summary <- ElapsedTime %>%
   rename(ElapsedTime = mean) %>% 
   filter(PortionC != 0)
 
-
 Q10q <- left_join(filter(ElapsedTime.summary, Treatment == "IB 20")[,c("PortionC", "Depth", "N", "ElapsedTime", "sd", "se")], 
                    filter(ElapsedTime.summary, Treatment == "IB 30")[,c("PortionC", "Depth", "N", "ElapsedTime", "sd", "se")], 
                    by = c("Depth", "PortionC"), 
@@ -98,59 +125,3 @@ Q10q <- left_join(filter(ElapsedTime.summary, Treatment == "IB 20")[,c("PortionC
   mutate(N = N.30 + N.20,
          se = dQ10q / sqrt(N)) %>% 
   select("PortionC", "Depth", "Q10q", "sd" = "dQ10q", "N", "se")
-
-ggplot(data = filter(Q10q, PortionC <=0.05), aes(x = PortionC, y = Q10q, fill = Depth))+
-  geom_errorbar(aes(min=Q10q-se, max=Q10q+se), width = 0.001)+
-  geom_line()+
-  geom_point(size = 4, shape = 21)+
-  labs(x = "Percent Cabon Respired", 
-       y = expression(Q[10-q]), 
-       fill = "Depth Horizon")+
-  coord_cartesian(xlim = c(0.01, 0.05))+
-  scale_x_continuous(labels = function(x) {paste0(round(x*100, 0), "%")})+
-  scale_y_continuous(breaks = c(1,2,3))+
-  scale_fill_manual(values = c("white", "gray20"))+
-  theme_bw()+
-  theme(panel.grid = element_blank())
-ggsave( "Q10-q rates for Fig 3_revised colors.tiff", dpi = 300, height = 3, width = 4, units = "in")
-
-ggplot(data = filter(Q10q, PortionC <=0.05), aes(x = PortionC, y = Q10q, shape = Depth))+
-  geom_errorbar(aes(min=Q10q-se, max=Q10q+se), width = 0.001)+
-  geom_line()+
-  geom_point(size = 4, fill = "black")+
-  labs(x = "Percent Cabon Respired", 
-       y = expression(Q[10-q]), 
-       fill = "Depth Horizon")+
-  coord_cartesian(xlim = c(0.01, 0.05))+
-  scale_x_continuous(labels = function(x) {paste0(round(x*100, 0), "%")})+
-  scale_y_continuous(breaks = c(1,2,3))+
-  scale_shape_manual(values = c(21, 24), labels = c("Deep", "Shallow"))+
-  theme_bw()+
-  theme(panel.grid = element_blank())
-ggsave( "Q10-q rates for Fig 3_revised shapes.tiff", dpi = 300, height = 3, width = 4, units = "in")
-
-ElapsedTime %>% 
-  filter(PortionC<=.1) %>% 
-ggplot(aes(PortionC, ElapsedTime, color=as.factor(Bottle)))+
-  geom_line()+
-  geom_point()+
-  facet_grid(Depth~Treatment)+
-  scale_x_continuous(limits = c(0.01, .1))+
-  labs(color="Bottle", 
-       x="Portion C Respired",
-       y="Incubation Time (days)")
-
-ElapsedTime.summary %>% 
-  filter(PortionC <= 0.1) %>% 
-ggplot(aes(PortionC, ElapsedTime, fill = Treatment, shape = Depth, group = paste(Depth, Treatment)))+
-  geom_errorbar(aes(min = ElapsedTime - se, max = ElapsedTime + se), width = 0.002, show.legend = F)+
-  geom_line( show.legend = F, color = "gray40") + 
-  geom_point(size = 5, aes(fill = Treatment, shape = Depth)) +
-  scale_fill_manual(values = c("gray90", "gray20"))+
-  scale_shape_manual(values = c(21, 24))+
-  scale_x_continuous(breaks = seq(0, .2, by= 0.02), labels =  function(x) paste0(x*100, "%"))+
-  labs(x = "Percent C Respired", 
-       y = "Incubation Time (days)", 
-       title = expression("Time to Respire Given Portion of Sediment Carbon"))+
-  theme_bw()+
-  guides(fill = guide_legend(override.aes = list(shape = c(22))))
